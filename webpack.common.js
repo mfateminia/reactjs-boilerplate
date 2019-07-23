93f286738c55fb4d93b9c0ed69ddd90044728464
Ali <mfateminia@gmail.com>
2019-07-23 22:20:43 +0200

    feat: typescript added and configured, works well

--- webpack.common.js
+++ webpack.common.js
@@ -35,7 +35,15 @@ module.exports = {
         test: /\.(js|jsx)$/,
         use: ['babel-loader'],
         exclude: /node_modules/
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   }
 }
